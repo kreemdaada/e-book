eb41ae10b652cda98b7b8a9814840cab36b286b4
kreemdaada <karmodaada1991@gmail.com>
2024-04-03 10:29:20 +0200

    PDF-Funktion-geaddet-bootstrap

--- resources/views/pdf/books.blade.php
+++ resources/views/pdf/books.blade.php
@@ -7,11 +7,17 @@
 
 </head>
 <body>
-    <h1>PDF Books</h1>
-    <ul>
-        @foreach($books as $book)
-            <li>{{ $book->title }}</li>
-        @endforeach
-    </ul>
+<div class="container">
+        <h1 class="mt-5 mb-4">PDF Books</h1>
+        <ul class="list-group">
+            @foreach($books as $book)
+            <li class="list-group-item">
+                <h4 class="font-weight-bold">{{ $book->title }}</h4>
+                <p><strong>Author:</strong> {{ $book->author }}</p>
+                <p>{{ $book->description }}</p>
+            </li>
+            @endforeach
+        </ul>
+    </div>
 </body>
 </html>
